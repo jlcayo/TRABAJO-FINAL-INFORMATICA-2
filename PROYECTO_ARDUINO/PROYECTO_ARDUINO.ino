ba034421daa5199edfa4c2b6116b499895957d28
Jose Cayo <josel.cayo@alumnos.frm.utn.edu.ar>
2025-12-09 11:49:13 -0300

    Mejoramos Comunicacion y agregamos buzzer

--- PROYECTO_ARDUINO/PROYECTO_ARDUINO.ino
+++ PROYECTO_ARDUINO/PROYECTO_ARDUINO.ino
@@ -1,49 +1,38 @@
-//probando cambios
 #include <SPI.h>          // Librería para comunicación SPI
 #include <MFRC522.h>      // Librería del lector RFID RC522
-
 #include <Servo.h>        //Libreria para el servo
 
+//pines de conexion
+#define buzzerPin 8
 #define SS_PIN 10          // Pin para SDA (selección de esclavo)
 #define RST_PIN 9          // Pin de reset del módulo RFID
 #define ledVerde 6        //Conectamos led rojo 
 #define ledRojo 7         //conectamos led verde
-
 #define Servopin 5    //servo conectado al pin 5
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Crea el objeto del lector RFID
-
 Servo servo;            //creamos el objeto del Servo
 
-struct tarjeta{
-  String id;
-  String nombre;
-  String cargo;
-};
-
-tarjeta tarjetasHabilitadas[15];
-int numTarjetas = 0;
-const String tarjetaMaestra = "432A4B1A";
+//antes teniamos Struct y String pero ya no es necesario
 
 void setup() {
   Serial.begin(9600);      // Inicia comunicación serial con la PC
   SPI.begin();             // Inicia comunicación SPI
   mfrc522.PCD_Init();      // Inicializa el lector RFID
-
   servo.attach(Servopin);   //Para inicializar servo 
   servo.write(0);   //Posicion inicial del Servo (Puerta Cerrada)
-
   pinMode(ledRojo, OUTPUT);
   pinMode(ledVerde, OUTPUT);
-
   digitalWrite(ledRojo, LOW);
   digitalWrite(ledVerde, LOW);
 
-  Serial.println("SISTEMA INICIADO");  // Mensaje de inicio
-  mostrarMenu();
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW);
+  Serial.println("SISTEMA INICIADO.");  // Mensaje de inicio
 }
 
 void loop() {
+  //revisamos si llego comando de processing
  if (Serial.available() > 0) {
     String comando = Serial.readStringUntil('\n');
     comando.trim();
@@ -51,50 +40,35 @@ void loop() {
       abrirPuerta();
     } else if (comando == "DENY") {
       digitalWrite(ledRojo, HIGH);
+      
+      for(int i=0; i<3; i++){
+       digitalWrite(buzzerPin, HIGH);
+       delay(100);
+       digitalWrite(buzzerPin, LOW);
+       delay(100);
+      }
+
       delay(3000);
       digitalWrite(ledRojo, LOW);
-    } else {
-      int opcion = comando.toInt();
-      if (opcion > 0) {
-        procesarComando(opcion);
-      }
-    }
+    }else if(comando == "1"){
+      //registrar tarjeta
+      registrarTarjeta();
+    }else if(comando == "2"){
+      //eliminar tarjeta
+      eliminarTarjeta();
+    }else if(comando == "3"){
+      //listar tarjetas, solo mensaje, porque processing maneja eso
+    } 
   }
 
   String idTarjeta = leerTarjeta();
   if (idTarjeta != "") {
-    verificarAcceso(idTarjeta);
+    //Mandamos UID a processing
+    Serial.println("UID:"+ idTarjeta);
   }
 }
 
-void mostrarMenu(){
-  Serial.println("---MENU---");
-  Serial.println("1. REGISTRAR TARJETA");
-  Serial.println("2. ELIMINAR TARJETA");
-  Serial.println("3. LISTAR TARJETAS");
-  Serial.println("Selecciona una opcion (1-3)");
-}
-
-void procesarComando(int opcion) {
-  switch (opcion) {
-    case 1:
-      registrarTarjeta();
-      break;
-    case 2:
-      eliminarTarjeta();
-      break;
-    case 3:
-      listarTarjetas();
-      break;
-    case 99:
-      abrirPuerta();
-      break;
-    default:
-      Serial.println("Opcion no valida, ingrese un numero del 1 al 3");
-      mostrarMenu();
-  }
-}
-
+//funcion para leer Tarjeta
 String leerTarjeta() {
   if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     return "";
@@ -107,24 +81,12 @@ String leerTarjeta() {
   }
   idTarjeta.toUpperCase();
   Serial.println("TARJETA DETECTADA");
+  
+  //liberamos la tarjeta para que se lea otra
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
   
   return idTarjeta;
-}
-
-void verificarAcceso(String id) {
-  Serial.println("UID: " + id);
-}
- 
-
-int buscarTarjeta(String id) {
-  for (int i = 0; i < numTarjetas; i++) {
-    if (tarjetasHabilitadas[i].id == id) {
-      return i;
-    }
-  }
-  return -1;
 }
 
 void registrarTarjeta() {
@@ -144,17 +106,6 @@ void registrarTarjeta() {
     delay(100);
   }
 
-  if (numTarjetas >= 15) {
-    Serial.println("NO SE PUEDEN AGREGAR MAS TARJETAS.");
-    return;
-  }
-
-  int indice = buscarTarjeta(idTarjeta);
-  if (indice != -1) {
-    Serial.println("ESTA TARJETA YA ESTA REGISTRADA");
-    return;
-  }
-
   Serial.println("INGRESE EL NOMBRE: ");
   while (Serial.available() == 0) {}
   String nombre = Serial.readStringUntil('\n');
@@ -165,15 +116,6 @@ void registrarTarjeta() {
   String cargo = Serial.readStringUntil('\n');
   cargo.trim();
 
-  tarjetasHabilitadas[numTarjetas].id = idTarjeta;
-  tarjetasHabilitadas[numTarjetas].nombre = nombre;
-  tarjetasHabilitadas[numTarjetas].cargo = cargo;
-  numTarjetas++;
-
-  Serial.print("TARJETA REGISTRADA A ");
-  Serial.print(nombre);
-  Serial.print(", ");
-  Serial.println(cargo);
   Serial.println("OK:" + idTarjeta + ":" + nombre + ":" + cargo);
 }
 
@@ -194,28 +136,14 @@ void eliminarTarjeta() {
     delay(100);
   }
   Serial.println("DEL:" + idTarjeta);
-  Serial.println("Tarjeta de " + idTarjeta + " ELIMINADA");
 }
 
 void listarTarjetas() {
   Serial.println("TARJETAS REGISTRADAS:");
-  if (numTarjetas == 0) {
-    Serial.println("NO HAY TARJETAS REGISTRADAS");
-  } else {
-    Serial.println("-------------");
-    for (int i = 0; i < numTarjetas; i++) {
-      Serial.print("ID: ");
-      Serial.println(tarjetasHabilitadas[i].id);
-      Serial.print("NOMBRE: ");
-      Serial.println(tarjetasHabilitadas[i].nombre);
-      Serial.print("CARGO: ");
-      Serial.println(tarjetasHabilitadas[i].cargo);
-      Serial.println("-------------");
-    }
-  }
 }
 
 void abrirPuerta() {
+  digitalWrite(buzzerPin, HIGH)
   digitalWrite(ledRojo, LOW);
   digitalWrite(ledVerde, HIGH);
   servo.write(75);
@@ -223,4 +151,5 @@ void abrirPuerta() {
   servo.write(0);
   digitalWrite(ledRojo, LOW);
   digitalWrite(ledVerde, LOW);
+  digitalWrite(buzzerPin, LOW);
 }
